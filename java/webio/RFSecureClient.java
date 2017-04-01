0b806f02fc97637861fa5e2f2c6e1b77b088fea1
Asimm Hirani <ahh200247718@gmail.com>
2017-04-01 09:25:07 -0400

    Updated SecureClient

--- java/webio/RFSecureClient.java
+++ java/webio/RFSecureClient.java
@@ -1,10 +1,18 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
+package webio;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.PostMethod;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 public class RFSecureClient {
 
@@ -14,72 +22,55 @@ public class RFSecureClient {
     String password;
 
     public int loginUser() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+            nvps.add(new BasicNameValuePair("username", username));
+            nvps.add(new BasicNameValuePair("password", password));
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+            CloseableHttpResponse response2 = httpclient.execute(httpPost);
 
-        //Instantiate an HttpClient
-        HttpClient client = new HttpClient();
-
-        //Instantiate a POST HTTP method
-        PostMethod method = new PostMethod(url);
-        method.setRequestHeader("Content-type",
-                "text/xml; charset=ISO-8859-1");
-
-        //Define name-value pairs to set into the QueryString
-        NameValuePair nvp1= new NameValuePair("username",username);
-        NameValuePair nvp2= new NameValuePair("password",password);
-
-        method.setQueryString(new NameValuePair[]{nvp1,nvp2});
-
-        try{
-            int statusCode = client.executeMethod(method);
-
-            System.out.println("Status Code = "+statusCode);
-            System.out.println("QueryString>>> "+method.getQueryString());
-            System.out.println("Status Text>>>"
-                  +HttpStatus.getStatusText(statusCode));
-
-            //Get data as a String
-            return Integer.parseInt(method.getResponseBodyAsString());
-
-            //release connection
-            method.releaseConnection();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
+            try {
+                System.out.println(response2.getStatusLine());
+                HttpEntity entity2 = response2.getEntity();
+                // do something useful with the response body
+                // and ensure it is fully consumed
+                EntityUtils.consume(entity2);
+            } finally {
+                response2.close();
+            }
+        } finally {
+            httpclient.close();
         }
     }
 
     public int checkInVisitor(String fName,
         String lName, String id, String keyID) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+            nvps.add(new BasicNameValuePair("username", username));
+            nvps.add(new BasicNameValuePair("password", password));
+            nvps.add(new BasicNameValuePair("firstName",fName));
+            nvps.add(new BasicNameValuePair("lName",lName));
+            nvps.add(new BasicNameValuePair("id",id));
+            nvps.add(new BasicNameValuePair("keyID",keyID));
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+            CloseableHttpResponse response2 = httpclient.execute(httpPost);
 
-        HttpClient client = new HttpClient();
-
-        //Instantiate a POST HTTP method
-        PostMethod method = new PostMethod(url);
-        method.setRequestHeader("Content-type",
-                "text/xml; charset=ISO-8859-1");
-
-        //Define name-value pairs to set into the QueryString
-        NameValuePair nvp1= new NameValuePair("username",username);
-        NameValuePair nvp2= new NameValuePair("password",password);
-
-        method.setQueryString(new NameValuePair[]{nvp1,nvp2});
-
-        try{
-            int statusCode = client.executeMethod(method);
-
-            System.out.println("Status Code = "+statusCode);
-            System.out.println("QueryString>>> "+method.getQueryString());
-            System.out.println("Status Text>>>"
-                  +HttpStatus.getStatusText(statusCode));
-
-            //Get data as a String
-            return Integer.parseInt(method.getResponseBodyAsString());
-
-            //release connection
-            method.releaseConnection();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
+            try {
+                System.out.println(response2.getStatusLine());
+                HttpEntity entity2 = response2.getEntity();
+                // do something useful with the response body
+                // and ensure it is fully consumed
+                EntityUtils.consume(entity2);
+            } finally {
+                response2.close();
+            }
+        } finally {
+            httpclient.close();
         }
     }
 }
